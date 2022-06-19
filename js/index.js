66ec7d713a88e2b6390847b66240df96b15b5d93
Brucebe <bebert@udesa.edu.ar>
2022-06-18 22:35:34 -0300

    nose que pasa con js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-
+window.addEventListener('load', function(){
 //Canciones populares
 fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
-.then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
-       console.log(data);
-       for(i=0; i<5; i++){
+    .then(function (data) {
+        console.log(data);
+        for (i = 0; i < 5; i++) {
 
-     document.getElementById('canciones').querySelector('ul').innerHTML +=
-        `
+            document.getElementById('canciones').querySelector('ul').innerHTML +=
+                `
         <li>
         <a class="index" href="./detallecanciones.html?id=${data.tracks.data[i].id}">
             <h4>${data.tracks.data[i].title}</h4>
@@ -17,13 +17,13 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
         <img class= "tamanoimg" src= "https://e-cdns-images.dzcdn.net/images/cover/${data.tracks.data[i].md5_image}/528x528-000000-80-0-0.jpg">
         </li>
         `
-       }
-//Albumes populares
+        }
+        //Albumes populares
 
-for(i=0; i<5; i++){
+        for (i = 0; i < 5; i++) {
 
-    document.getElementById('albumes').querySelector('ul').innerHTML +=
-       `
+            document.getElementById('albumes').querySelector('ul').innerHTML +=
+                `
        <li>
        <a class="index" href="./detallecanciones.html?id=${data.albums.data[i].id}">
            <h4>${data.albums.data[i].title}</h4>
@@ -31,12 +31,12 @@ for(i=0; i<5; i++){
        <img class= "tamanoimg" src= "https://e-cdns-images.dzcdn.net/images/cover/${data.albums.data[i].md5_image}/528x528-000000-80-0-0.jpg">
        </li>
        `
-      }
+        }
 
-      for(i=0; i<5; i++){
+        for (i = 0; i < 5; i++) {
 
-        document.getElementById('artistas').querySelector('ul').innerHTML +=
-           `
+            document.getElementById('artistas').querySelector('ul').innerHTML +=
+                `
            <li>
            <a class="index" href="./detallecanciones.html?id=${data.artists.data[i].id}">
                <h4>${data.artists.data[i].name}</h4>
@@ -44,16 +44,12 @@ for(i=0; i<5; i++){
            <img class= "tamanoimg" src= "${data.artists.data[i].picture}">
            </li>
            `
-          }
+        }
 
     })
 
-    
-
-
-
-    .catch(function(error){
+    .catch(function (error) {
         console.log(error);
     })
 
-    
+})
