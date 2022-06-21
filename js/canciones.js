c1da261746ab18a05973cd1fffd58f2562c71c1b
FelixAlexander02 <falexander@udesa.edu.ar>
2022-06-20 23:34:03 -0300

    terminando playlist

--- js/canciones.js
+++ js/canciones.js
@@ -18,9 +18,9 @@ fetch(urlDetalleCancion)
     let playercancion = document.querySelector('.player')
 
     detalleimg.src = `https://e-cdns-images.dzcdn.net/images/cover/${data.md5_image}/528x528-000000-80-0-0.jpg`;
-    nombrecancion.innerText = data.title;
-    nombreartista.innerText = data.artist.name;
-    nombredisco.innerText = data.album.title; 
+    nombrecancion.innerText = 'Nombre de la canción: ' + data.title;
+    nombreartista.innerText = 'Nombre del artista: ' + data.artist.name;
+    nombredisco.innerText = 'Nombre del album: ' + data.album.title; 
     playercancion.src = `https://widget.deezer.com/widget/dark/track/${idCancion}`
     
    
@@ -40,20 +40,36 @@ fetch(urlDetalleCancion)
 
     let link= document.querySelector(".addplaylist"); 
 
+    if(favoritos.includes(idCancion)){
+        //cambiarle el mensaje al usuario
+        link.innerText = "Sacar de la playlist"
+    } // pregunto si el id esta, le voy a cambiar el texto
+
     //Definir evento para el link 
     link.addEventListener("click", function(evento){    // evento es un parametro, cuando lo use adentro hay q respetar lo q pusimos aca
         //evitar default del link
         evento.preventDefault()  // frenar el link
 
-        //Agregar un data al array 
-        favoritos.push(idCancion)
+        if(favoritos.includes(idCancion)){
+            //Sacar el id del array
+            let gifASacar = favoritos.indexOf(idCancion) // un gif a sacar que sabemos q esta en esta posicion 
+            favoritos.splice(gifASacar, 1);     // splice es el metodo que saca, gifASacar es la posicion que encontramos y cuantos queremos sacar
 
-        //Agregar el array a localStorage. Antes hay que pasarlo a string
-        let cancionFavoritosToString = JSON.stringify(favoritos);
+            link.innerText = "Agregar a playlist"
+
+        } else {
+            //Agregar un data al array 
+            favoritos.push(idCancion)
+            //cambiarle el mensaje al usuario
+            link.innerText = "Sacar de la playlist"
+        } // cuando haga click en el link voy a preguntar si tengo el id adentro del array, si esta, el codigo lo va a sacar y le va a volver a cambiar el texto a agregar a fav, y si no, se pushea adentro del array 
+
+
+        //Agregar el array a localStorage. Antes hay que pasarlo a string //por fuera del else, lo q va a pasar siempre es q voy a stringifear lo q sea q tenga en el array y gurdarlo en local storage
+        let cancionFavoritosToString = JSON.stringify(favoritos); // siempre vamos aestrigifiear el array, osea pasarlo a JSON, y despues lo guardamos en local sotrage en la posicion q querramos, en este caso en favoritos 
         localStorage.setItem('favoritos', cancionFavoritosToString) //el favoritos es= dentro de mi objeto literal donde lo voy a guardar, y el segundo parametro son los datos 
  
         console.log(localStorage);
-        //Minuto 40 del video 
     })
 
  })
--- js/playlist.js
+++ js/playlist.js
@@ -3,3 +3,45 @@
 // Meterlo en un array
 // Guardarlo en local storage 
 
+let favoritos = [];
+//recuperar el array del storage 
+let recuperoStorage = localStorage.getItem("favoritos");
+let recuperoStorageToArray = JSON.parse(recuperoStorage);
+
+favoritos = recuperoStorageToArray
+
+//antes del for, capturar la seleccion para completar con datos 
+let section = document.querySelector(".cancionesplaylist")
+let contenidoSection  = ""
+
+//pedir a la api los datos de las canciones 
+for(let i=0; i<favoritos.length ; i++){
+    //fetch
+    let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${favoritos[i]}`
+
+    fetch(url)  // necesito remplazar ese id por algo 
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+    //aca trabajo con los datos 
+        contenidoSection += `<article>
+                                <a class="index" href="./detallecanciones.html?id=${data.tracks.data[i].id}">
+                                <h4>${data.title}</h4>
+    
+                                <img class= "tamanoimg" src= "https://e-cdns-images.dzcdn.net/images/cover/${data.md5_image}/528x528-000000-80-0-0.jpg">
+                                </a>
+                            </article>`
+                            section.innerHTML = contenidoSection //pasar todos los datos al dom
+    })
+    .catch(function(error){
+    // el q nos va a mostrar los errores 
+        console.log(error);
+    })
+} // tengo un for, que va a pasar por cada pocision de mi array, cada vez q yo pase por una posicion del array voy a ir creando la url, esa url va a pedir datos y me los va a retornar adentro del fetch. y despues quiero guardar cada dato del fetch.
+// antes del for-->, adentro del for rellenarla, y al final pasar todos los datos al DOM
+
+
+
+//necesitamos ejecutar este fetch, adentro de un bucle, porque tengo q ir remplazando los ids por cada uno que tengo en el array .
+//por ende, antes de todo necesito un for que recorra mi array. osea i=0 , despues pasarle i menor a recuperostoragetoarray.length, y por ultimo i++
